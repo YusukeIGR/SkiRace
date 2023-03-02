860f4a7039614d21ecf1f6a06e2824afbaa5b270
YusukeIGR <javakunren84@gmail.com>
2023-03-02 13:32:26 +0900

    4時間目

--- Assets/Script/AddForce.cs
+++ Assets/Script/AddForce.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
  
 public class AddForce : MonoBehaviour {
- 
+    private Vector3 latestPos; 
     //一秒間に一定の回数呼ばれる
     void FixedUpdate()
     {
@@ -16,5 +16,15 @@ public class AddForce : MonoBehaviour {
  
         //Rigidbodyに力を加える
         rb.AddForce(x, 0, z);
+
+        //変更点
+        Vector3 diff = transform.position - latestPos;   //前回からどこに進んだかをベクトルで取得
+        latestPos = transform.position;  //前回のPositionの更新
+
+        //ベクトルの大きさが0.01以上の時に向きを変える処理をする
+        if (diff.magnitude > 0.01f)
+        {
+        transform.rotation = Quaternion.LookRotation(diff); //向きを変更する
+        }
     }
 }
